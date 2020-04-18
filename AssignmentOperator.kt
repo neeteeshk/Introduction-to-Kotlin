6dd234f9f428f3d22f0ff08e425de5120bfbc5b6
neeteeshk <40706352+neeteeshk@users.noreply.github.com>
2020-04-18 13:19:49 +0530

    Add files via upload

---
+++ AssignmentOperator.kt
@@ -0,0 +1,27 @@
+//Assign values to operand  a=b
+fun main()
+{
+    var number1=12.5
+    val number2=2.5
+
+    number1+=number2
+    println(number1);
+
+    number1=12.5
+    number1-=number2
+    println(number1);
+
+    number1=12.5
+    number1*=number2
+    println(number1);
+
+
+    number1=12.5
+    number1/=number2
+    println(number1);
+
+    number1=11.5
+    number1%=number2
+    println(number1);
+
+}
