500476c4b290a4428ebe63d054fb1e633ff447ce
neeteeshk <40706352+neeteeshk@users.noreply.github.com>
2020-04-18 21:54:38 +0530

    Add files via upload

---
+++ Question2.kt
@@ -0,0 +1,13 @@
+/*
+Compare the length of two strings, a = “Internshala” and b = “Trainings” and print “length is same”
+ if it is same and print “Length is different” if it is different.
+ */
+fun main()
+{
+    val str1="Internshala"
+    val str2="Trainings"
+    if(str1.length==str2.length)
+        println("length is same")
+    else
+        println("Length is different")
+}
---
+++ Question3.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // write your code here
+    val num=100
+    if(num>=100)
+        println("number is greater than or equal to 100")
+    else if(num<100 && num>50)
+        println("number is between 50-100")
+    else if(num<=50)
+        println("number is less than or equal to 50")
+}
