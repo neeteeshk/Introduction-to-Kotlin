0986fb4c5606748006cf1a9274965a672f7467a9
neeteeshk <40706352+neeteeshk@users.noreply.github.com>
2020-04-19 08:18:53 +0530

    Add files via upload

---
+++ loops2.kt
@@ -0,0 +1,10 @@
+//try to calculate the sum of the first 100 natural numbers using the concept of loops.
+fun main()
+{
+    var sum=0
+    for (i in 1..100)
+    {
+        sum+=i
+    }
+    println(sum)
+}
