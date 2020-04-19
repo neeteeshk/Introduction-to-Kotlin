fc57cad6374b0af5f8be22f2631e1896d4ad1eb6
neeteeshk <40706352+neeteeshk@users.noreply.github.com>
2020-04-19 08:32:00 +0530

    Add files via upload

---
+++ loop3.kt
@@ -0,0 +1,24 @@
+fun main()
+{
+    for (i in 1..10 step 2)
+    {
+        println(i)
+    }
+
+
+    var i=1
+    while(i<=10)
+    {
+        println(i)
+        i+=2
+    }
+
+
+    i=1
+    do{
+        println(i)
+        i+=2
+    }while (i<=10)
+
+    
+}
---
+++ loops4.kt
@@ -0,0 +1,23 @@
+fun main()
+{
+    for (i in 10 downTo 1)
+    {
+        println(i)
+    }
+
+
+    var i=10
+    while(i>0)
+    {
+        println(i)
+        i--
+    }
+
+
+
+    i=10
+    do{
+        println(i)
+        i--
+    }while (i>0)
+}
